d5df363cd95b5f0d87b8d7f2ec734f18f10c250b
Marley Barrett <marleyhansenbarrett@icloud.com>
2026-05-17 14:15:09 -0500

    fix: force-dynamic on (app) layout to prevent static prerender of cookie-dependent pages

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { redirect } from 'next/navigation'
 import { getCurrentUser } from '@/lib/auth/get-current-user'
 import AppNav from '@/components/app-nav'
